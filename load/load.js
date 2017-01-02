d53b45927c4da69eb823204fe65884c81800710d
MRbcgd <quittypig@gmail.com>
2017-01-02 17:19:18 +0900

    html에 js코드 load, head에 외부파일로 분리 방식

--- load/load.js
+++ load/load.js
@@ -1,4 +1,7 @@
-var hw=document.getElementById('hw');
-hw.addEventListener('click',function(){
-    alert('Hello World');
-})
+//head에 load 될 경우
+window.onload=function(){
+  var hw=document.getElementById('hw');
+  hw.addEventListener('click',function(){
+      alert('Hello World');
+  })
+}
